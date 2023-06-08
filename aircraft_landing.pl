bb7d553c45a925b3b11f7e4bcc711ea885e61904
Miguel Lopes <lopesazevedom@gmail.com>
2023-06-08 23:50:12 +0100

    formatting files

--- aircraft_landing.pl
+++ aircraft_landing.pl
@@ -26,7 +26,7 @@
 
 aircraft_landing:-
 % see('/home/miguel/Documents/Faculdade/PLR/FEUP-PLR/airland1.txt'),
-see('/Users/mafalda/Documents/FEUP/PLR/FEUP-PLR/data/airland8.txt'),
+see('/home/miguel/Documents/Faculdade/PLR/FEUP-PLR/data/airland2.txt'),
 first_line_process(NumberPlanes,_),
 remaining_lines_process(NumberPlanes,_,EarliestLandingTimes,TargetLandingTimes,LatestLandingTimes,PenaltyBefore,PenaltyAfter,SeparationTimes),
 seen,!,
@@ -43,7 +43,8 @@ relate_times_before_and_after(TimesBefore,TimesAfter,TargetLandingTimes,LandingT
 scalar_product(PenaltyBefore,TimesBefore,#=,FirstVal),
 scalar_product(PenaltyAfter,TimesAfter,#=,SecondVal),
 sum([FirstVal,SecondVal],#=,Sum),
-labeling([minimize(Sum),time_out(600000,Flag),up,step,impact],LandingTimes),
+write(LandingTimes),
+labeling([minimize(Sum),time_out(10000,Flag),impact,step,up],LandingTimes),
 statistics(runtime, [End|_]),
 ExecutionTime is End-Start,
 nl,write('Sum: '),write(Sum),nl,write('Times After: '),write(TimesAfter),nl,write('Times Before: '),write(TimesBefore),nl,write('Execution Time: '),write(ExecutionTime),nl,write(Flag).
