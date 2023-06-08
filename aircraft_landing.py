2e7270635864bfe5f71e17432fac77ed492211ea
Mafalda Magalhães <mafalda@MBP-de-Mafalda.home>
2023-06-08 16:22:25 +0100

    OR tools working

--- aircraft_landing.py
+++ aircraft_landing.py
@@ -3,8 +3,7 @@ from ortools.sat.python import cp_model
 
 def aircraft_landing():
     # Read data from the file
-    # with open('/home/miguel/Documents/Faculdade/PLR/FEUP-PLR/airland1.txt', 'r') as file:
-    with open('airland1_copy.txt', 'r') as file:
+    with open('airland1.txt', 'r') as file:
         lines = file.readlines()
 
     # Extract values from the first line
@@ -36,94 +35,41 @@ def aircraft_landing():
         
         current_line+=1
 
-    # print("Appearence Times :")
-    # print(appearance_times)
-    # print("Earliest Landing Times :")
-    # print(earliest_landing_times)
-    # print("Target Landing Times :")
-    # print(target_landing_times)
-    # print("Latest Landing Times :")
-    # print(latest_landing_times)
-    # print("Penalty Before :")
-    # print(penalty_before)
-    # print("Penalty After :")
-    # print(penalty_after)
-    # print("Separation Times :")
-    # print(separation_times)
-
     model = cp_model.CpModel()
     landing_times = [model.NewIntVar(earliest_landing_times[i], latest_landing_times[i], f"LandingTime{i+1}") for i in range(number_planes)]
 
-    # Earliest and latest landing times constraints
-    # Each plane must land within its specified time window: Ei ≤ xi ≤ Li for all planes i.
-    # for i in range(number_planes):
-    #     model.Add(landing_times[i] >= earliest_landing_times[i])
-    #     model.Add(landing_times[i] <= latest_landing_times[i])
-
     model.AddAllDifferent(landing_times)
-
-
-#   For each pair of planes (i, j), either plane i lands before plane j (δij = 1) or plane j lands before plane i (δji = 1).
-#   There are three sets of plane pairs:
-#       a) Set W: i must land before j (Li < Ej) and the separation constraint is automatically satisfied (Li + Sij ≤ Ej).
-#        b) Set V: i must land before j (Li < Ej), but the separation constraint is not automatically satisfied (Li + Sij > Ej).
-#       c) Set U: pairs with overlapping time windows where uncertainty exists about the order of landing.
-#   δij = 1 for all pairs (i, j) in set W or V.
-#   Separation constraint for pairs in set V: xj ≥ xi + Sij.
-#   Separation constraint for pairs in set U: xj ≥ xi + Sij - (Li + Sij - Ej)δji.
     
     is_before = []
-    is_after = []
 
     for i in range(number_planes):
         current_before = []
-        current_after = []
 
         for j in range(number_planes):
-            current_before.append(model.NewIntVar(0, 1, f"before_{i}_{j}"))
-            current_after.append(model.NewIntVar(0, 1, f"after_{i}_{j}"))
+            current_before.append(model.NewBoolVar(f"before_{i}_{j}"))
 
         is_before.append(current_before)
-        is_after.append(current_after)
-
-    for i in range(number_planes):
-        for j in range(number_planes):
-            if i != j:
-                model.Add(is_before[i][j] + is_after[i][j] == 1)
-                model.Add(is_before[i][j] + is_before[j][i] == 1)
-                model.Add(is_after[i][j] + is_after[j][i] == 1)
-            else:
-                model.Add(is_before[i][j] == 0)
-                model.Add(is_after[i][j] == 0)
-    
-    # print("Model -", model)
 
     # Separation constraints
     for i in range(number_planes):
-        for j in range(i+1, number_planes):
-            # i must land before j
-            if latest_landing_times[i] < earliest_landing_times[j]:
-                model.Add(landing_times[j] >= landing_times[i] + separation_times[i][j])
-            # If the time windows of i and j overlap, ensure separation time is met
-            else:
-                model.Add((landing_times[i] * is_after[i][j]) + (landing_times[j] * is_after[j][i]) >= separation_times[i][j] + (landing_times[i] * is_before[i][j]) + (landing_times[j] * is_before[j][i]))
+        for j in range(i, number_planes):
+            if i != j:
+                # i must land before j
+                if latest_landing_times[i] < earliest_landing_times[j]:
+                    model.Add(landing_times[j] >= landing_times[i] + separation_times[i][j])
+                # If the time windows of i and j overlap, ensure separation time is met
+                else:
+                    model.Add(landing_times[i] >= landing_times[j] + separation_times[j][i]).OnlyEnforceIf(is_before[j][i])
+                    model.Add(landing_times[j] >= landing_times[i] + separation_times[i][j]).OnlyEnforceIf(is_before[i][j])
 
-
-
-                
-    # print("Model - ", model)
+                    model.AddExactlyOne([is_before[j][i], is_before[i][j]])
 
     time_before = [model.NewIntVar(0, target_landing_times[i] - earliest_landing_times[i], f"TimesBefore{i+1}") for i in range(number_planes)]
     time_after = [model.NewIntVar(0, latest_landing_times[i] - target_landing_times[i], f"TimesAfter{i+1}") for i in range(number_planes)]
-
-    # print("Model - ", model)
     
     # Time before and after target constraint
     for i in range(number_planes):
         model.AddAbsEquality(time_before[i] + time_after[i], landing_times[i] - target_landing_times[i])
-        # model.Add(time_before[i] + time_after[i] == landing_times[i] - target_landing_times[i])
-
-    # print("Model - ", model)
 
     # Objective function
     objective_expr = sum(
